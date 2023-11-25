d908633e8b6864e056893955c7a94e01a71b3036
Electroma <amitleshem+1@gmail.com>
2023-11-25 20:12:45 +0200

    CSS changes

--- client/src/scenes/global/Topbar.jsx
+++ client/src/scenes/global/Topbar.jsx
@@ -1,24 +1,16 @@
 import { Box, IconButton, useTheme } from "@mui/material";
 import { useContext } from "react";
-import { ColorModeContext, tokens } from "../../theme";
-import InputBase from "@mui/material/InputBase";
+import { ColorModeContext } from "../../theme";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
-import SearchIcon from "@mui/icons-material/Search";
 import AccountMenu from "./AccountMenu";
 
 const Topbar = ({ user }) => {
     const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
     const colorMode = useContext(ColorModeContext);
     return (
         <Box display="flex" justifyContent="space-between" p={ 2 }>
-            <Box display="flex" backgroundColor={ colors.primary[400] } borderRadius="3px">
-                <InputBase sx={ { ml: 2, flex: 1 } } placeholder="Search"/>
-                <IconButton type="button" sx={ { p: 1 } }>
-                    <SearchIcon/>
-                </IconButton>
-            </Box>
+            <Box display="flex" />
 
             <Box display="flex">
                 <IconButton onClick={ colorMode.toggleColorMode }>

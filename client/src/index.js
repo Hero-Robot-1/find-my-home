9a3dc506199955ce34d907a49ef5318d1eef93c5
Amit Leshem <admin@MacBook-Pro.local>
2026-05-10 12:30:27 +0300

    Add docker-compose, nginx config, and updated CI/CD pipeline for AWS deployment

--- client/src/index.js
+++ client/src/index.js
@@ -11,5 +11,5 @@ root.render(
     </HashRouter>
 );
 export const serverUrl = () => {
-    return process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://find-my-home-server.onrender.com'
+    return process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : process.env.REACT_APP_API_URL
 }
